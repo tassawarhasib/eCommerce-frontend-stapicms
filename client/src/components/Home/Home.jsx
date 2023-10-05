2801b67584c31b9def72a35beca5d42644e72df5
Tassawar hasib <nabilhasib840@gmail.com>
2023-10-06 02:33:54 +0530

    Category section completed, hardcoded the products for now

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,13 +1,19 @@
 import "./Home.scss";
 
 import Banner from "./Banner/Banner";
+import Category from "../Category/Category";
 
 const Home = () => {
-    return (
-    <div className="home">
-        <Banner/>
+  return (
+    <div>
+      <Banner />
+      <div className="main-content">
+        <div className="layout">
+          <Category />
+        </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Home;
