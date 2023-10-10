9cf64fd1bcf0507b80d172ecbcb0990539644a5f
Tassawar hasib <nabilhasib840@gmail.com>
2023-10-11 01:47:14 +0530

    completed cart and search components, Fronted completed final push, going forward to implement backend

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -12,43 +12,51 @@ import { Context } from "../../utils/context";
 import "./Header.scss";
 
 const Header = () => {
-    const [scrolled, setScrolled] = useState(false);
-    const handleScroll = () =>{
-        const offset = window.scrollY;
-        console.log(offset);
-        if(offset>200){
-            setScrolled(true);
-        }
-        else{
-            setScrolled(false);
-        }
-    };
+  const [scrolled, setScrolled] = useState(false);
+  const [showCart, setShowCart] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    console.log(offset);
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
 
-    useEffect(()=>{
-        window.addEventListener("scroll" , handleScroll)
-    }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <header className={`main-header ${scrolled ? 'sticky-header' : ''}`}>
-      <div className="header-content">
-        <ul className="left">
-          <li>Home</li>
-          <li>About</li>
-          <li>Categories</li>
-        </ul>
+    <>
+      <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+        <div className="header-content">
+          <ul className="left">
+            <li>Home</li>
+            <li>About</li>
+            <li>Categories</li>
+          </ul>
 
-        <div className="center">Power Discount</div>
+          <div className="center">Power Discount</div>
 
-        <div className="right">
-          <TbSearch />
-          <AiOutlineHeart />
-          <span className="cart-icon">
-            <CgShoppingCart />
-            <span>5</span>
-          </span>
+          <div className="right">
+            <TbSearch onClick={() => setShowSearch(true)}/>
+            <AiOutlineHeart />
+            <span
+              className="cart-icon"
+              onClick={() => setShowCart(true)}
+            >
+              <CgShoppingCart />
+              <span>5</span>
+            </span>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      {showCart && <Cart setShowCart={setShowCart} />}
+      {showSearch && <Search setShowSearch={setShowSearch} />}
+    </>
   );
 };
 
--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -1,6 +1,33 @@
+import { MdClose } from "react-icons/md";
+import prod from "../../../assets/products/earbuds-prod-1.webp";
+
 import "./Search.scss";
-const Search = () => {
-    return <div>Search</div>;
+const Search = ({ setShowSearch }) => {
+    return (
+        <div className="search-modal">
+            <div className="form-field">
+                <input
+                    type="text"
+                    autoFocus
+                    placeholder="Search for products"
+                />
+                <MdClose onClick={() => setShowSearch(false)} />
+            </div>
+            <div className="search-result-content">
+                <div className="search-result">
+                    <div className="search-result-item">
+                        <div className="img-container">
+                            <img src={prod} alt="" />
+                        </div>
+                        <div className="prod-details">
+                            <span className="name">Product name</span>
+                            <span className="desc">Product desc</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Search;
